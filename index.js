1f2ae503e213082de04b46d84d99ec9235db78e0
SURAJ-acube <suraj@acubeapp.com>
2025-03-20 20:26:13 +0530

    fixed hosting

--- index.js
+++ index.js
@@ -1,73 +1,76 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Menu = require('./models/menu.model');
-require('dotenv').config();
+const Menu = require("./models/menu.model");
+require("dotenv").config();
 const mongoURI = process.env.MONGO_URI;
 
 const app = express();
 
 app.use(express.json());
 
-mongoose.connect(
-    mongoURI
-  ).then(()=>{
+mongoose
+  .connect(mongoURI)
+  .then(() => {
     console.log("connected database");
-    app.listen(3000,()=>{
-        console.log('server running')
-    })
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
   });
 
-  app.get('/',(req, res) => {
-    console.log('server running')
-  });
+app.get("/", (req, res) => {
+  console.log("server running");
+});
 
-  app.get('/api/menus', async (req, res) => {
-    try {
-      const menus = await Menu.find({},{menuName:1}); 
-      res.json(menus);
-    } catch (error) {
-      res.status(500).send('Error fetching menus');
+app.get("/api/menus", async (req, res) => {
+  try {
+    const menus = await Menu.find({}, { menuName: 1 });
+    res.json(menus);
+  } catch (error) {
+    res.status(500).send("Error fetching menus");
+  }
+});
+
+app.get("/api/items/:menuId", async (req, res) => {
+  const { menuId } = req.params;
+  try {
+    const menu = await Menu.findById(menuId);
+    if (!menu) {
+      return res.status(404).send("Menu not found");
     }
-  });
+    res.json(menu.items);
+  } catch (error) {
+    res.status(500).send("Error fetching menu items");
+  }
+});
 
-  app.get('/api/items/:menuId', async (req, res) => {
-    const { menuId } = req.params;
-    try {
-      const menu = await Menu.findById(menuId);
-      if (!menu) {
-        return res.status(404).send('Menu not found');
-      }
-      res.json(menu.items); 
-    } catch (error) {
-      res.status(500).send('Error fetching menu items');
-    }
-  });
+app.post("/api/menus", async (req, res) => {
+  const { menuName, description } = req.body;
+  try {
+    const newMenu = await Menu.insertOne({ menuName, description, items: [] });
+    res.status(201).json(newMenu);
+  } catch (error) {
+    res.status(500).send("Error creating menu");
+  }
+});
 
-  app.post('/api/menus', async (req, res) => {
-    const { menuName, description } = req.body;
-    try {
-      const newMenu = await Menu.insertOne({ menuName, description, items: [] });
-      res.status(201).json(newMenu);
-    } catch (error) {
-      res.status(500).send('Error creating menu');
-    }
-  });
+app.post("/api/items/:menuId", async (req, res) => {
+  const { menuId } = req.params;
+  const { name, description, price } = req.body;
+  console.log(menuId);
 
-  app.post('/api/items/:menuId', async (req, res) => {
-    const { menuId } = req.params;
-    const { name, description, price } = req.body;
-    console.log(menuId);
-  
-    try {
-      const newItem = { name, description, price };
-      const result = await Menu.updateOne({ _id: menuId },{$push:{items:newItem}});
-      res.status(201).json(result);
-    } catch (error) {
-        console.log(error)
-      res.status(500).send('Error adding menu item');
-    }
-  });
-
-  
+  try {
+    const newItem = { name, description, price };
+    const result = await Menu.updateOne(
+      { _id: menuId },
+      { $push: { items: newItem } }
+    );
+    res.status(201).json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error adding menu item");
+  }
+});
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log("server running");
+});
